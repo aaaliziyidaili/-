b1f33a2e607575ec0f95927584d9d1a5d5274393
aaaliziyidaili <165760594+aaaliziyidaili@users.noreply.github.com>
2024-06-26 00:22:56 +0800

    Add files via upload

---
+++ --main/江苏/script.js
@@ -0,0 +1,35 @@
+// 获取DOM元素  
+const heroSection = document.querySelector('.hero');  
+const navLinks = document.querySelectorAll('nav ul li a');  
+  
+// 滚动到顶部时改变导航链接的样式 
+window.addEventListener('scroll', function() {  
+    if (window.pageYOffset > 50) {  
+        document.body.style.paddingTop = '70px';
+        navLinks.forEach(link => link.classList.add('active'));  
+    } else {  
+        document.body.style.paddingTop = '0';  
+        navLinks.forEach(link => link.classList.remove('active'));  
+    }  
+});  
+  
+// 添加点击事件到导航链接，使其在页面内部滚动  
+navLinks.forEach(function(link) {  
+    link.addEventListener('click', function(e) {  
+        e.preventDefault(); 
+        const sectionId = this.getAttribute('href').substring(1); 
+        const section = document.getElementById(sectionId);  
+        if (section) {  
+            scrollToSection(section); 
+        }  
+    });  
+});  
+  
+// 自定义滚动函数  
+function scrollToSection(section) {  
+    const offset = -50; 
+    window.scrollTo({  
+        top: section.getBoundingClientRect().top + window.pageYOffset + offset,  
+        behavior: 'smooth' 
+    });  
+}  
